5bc31ea81a70f9980a40be4bb6e028000af7a389
Thyago Piske <thyagopiske@gmail.com>
2024-04-03 12:28:21 -0300

    undo conn error

--- src/server/socket.ts
+++ src/server/socket.ts
@@ -24,10 +24,4 @@ export function configureSockets(io: Server): void {
     }, 1000)
   });
 
-  io.engine.on("connection_error", (err) => {
-    console.log(err.req);      // the request object
-    console.log(err.code);     // the error code, for example 1
-    console.log(err.message);  // the error message, for example "Session ID unknown"
-    console.log(err.context);  // some additional error context
-  });
 }
