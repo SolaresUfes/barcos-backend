012d457518622d4256167161d856a1d60a7958e5
naascimento0 <gabrieloliveiran@hotmail.com>
2025-03-31 20:21:25 -0300

    Atualiza a configuração do ChatGPT

--- src/utils/chatgpt.ts
+++ src/utils/chatgpt.ts
@@ -1,10 +1,11 @@
 import { Response } from 'express';
-import { Configuration, OpenAIApi } from 'openai'
+import OpenAI from 'openai'; 
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const configuration = new Configuration({ apiKey: process.env.OPENAI_KEY });
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+});
 
 export async function gptResponse(promptText: string, res: Response) {
   if(!promptText) {
@@ -14,39 +15,27 @@ export async function gptResponse(promptText: string, res: Response) {
   console.log('Prompt text', promptText);
 
   try {
-    const completion = await openai.createChatCompletion({
+    const stream = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
-      messages: [{role: "user", content: promptText}],
+      messages: [{ role: 'user', content: promptText }],
       temperature: 0.9,
-      stream: true,
-    }, { responseType: 'stream' }) as any;
-  
-    completion.data.on('data', (data: any) => {
-      const lines = data.toString().split('\n').filter((line: string) => line.trim() !== '');
-      for (const line of lines) {
-        const message = line.replace(/^data: /, '');
-        // console.log(message)
-        if (message === '[DONE]') {
-          console.log('Stream finished');
-          return res.end();
-        }
-        try {
-          const parsed = JSON.parse(message);
-          const content = parsed.choices[0].delta.content;
-          console.log(content);
-          if(content) {
-            // res.write(`data: ${content}\n\n`);
-            res.write(content);
-          }
-        } catch (error) {
-          console.error('Could not JSON parse stream message', message, error);
-          throw error;
-        }
+      stream: true, // Habilitar streaming
+    });
+
+    for await (const chunk of stream) {
+      const content = chunk.choices[0]?.delta?.content;
+      if (content) {
+        console.log(content);
+        res.write(content);
       }
-    })
-  }
-  catch (error) {
+    }
+
+    console.log('Stream finished');
+    res.end();
+  } catch (error) {
     console.error('Error in gptResponse', error);
+    res.write('Erro ao processar a requisição. Tente novamente mais tarde.');
+    res.end();
     throw error;
   }
 }
